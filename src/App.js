6ec75ba5d6999e95fc4c598093820fcc38cb8ecd
George Arnold <george.arnold.88@gmail.com>
2020-01-06 08:54:12 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: robots,
-      searchfield: ""
-    };
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ 
-      searchfield: event.target.value })
-  }
+  // constructor() {
+  //   super()
+  //   this.state = {
+     
+  //   };
+  // }
   render() {
-    const filteredRobots = this.state.robots.filter(robot=> {
-    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
+  
     return (
-      <div className="tc">
-        <h1> RoboFriends </h1>
-        <SearchBox searchChange = {this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-      </div>
+      <main>
+        <h1> Test </h1>
+      </main>
     );
   }
 }
